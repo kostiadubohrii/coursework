86797cfac2e876168acb28a5a78827341e5a0ef5
= <kostadubogriy@gmail.com>
2023-07-12 10:27:29 +0100

    user model added

--- backend/backend_api/users/models.py
+++ backend/backend_api/users/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=210)
-    password = models.CharField(max_length=200)
+    name = models.CharField(max_length=210, default=None)
+    last_name= models.CharField(max_length=210, default=None)
+    is_loggedin = models.BooleanField(default=False)
+    email = models.EmailField(max_length = 254, default=None)
+    password = models.CharField(max_length=200, default=None)
 
     def __str__(self):
         return self.name

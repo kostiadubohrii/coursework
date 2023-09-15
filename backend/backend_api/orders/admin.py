8380cdf03aa083e26276a14a26cbcc31866354d8
= <kostadubogriy@gmail.com>
2023-09-15 09:20:06 +0100

    users, orders admin panel update

--- backend/backend_api/orders/admin.py
+++ backend/backend_api/orders/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from orders.models import *
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Order._meta.fields]
 
-admin.site.register(Order)
+    class Meta:
+        model = Order
+admin.site.register(Order, OrderAdmin)
--- backend/backend_api/users/admin.py
+++ backend/backend_api/users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import User
+class UserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in User._meta.fields]
 
-admin.site.register(User)
+    class Meta:
+        model = User
+admin.site.register(User, UserAdmin)
